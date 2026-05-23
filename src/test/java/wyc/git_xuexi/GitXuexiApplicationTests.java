ae6e9cc0c33760183f627e3cddff59af62aa7681
wyc <3361135241@qq.com>
2026-05-24 00:15:49 +0800

    第一次提交

---
+++ src/test/java/wyc/git_xuexi/GitXuexiApplicationTests.java
@@ -0,0 +1,13 @@
+package wyc.git_xuexi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitXuexiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
