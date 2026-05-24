73692aa4d51e648ffd608dd83428b3f48def7f12
wyc <3361135241@qq.com>
2026-05-24 15:25:20 +0800

    删除

--- src/main/java/wyc/git_xuexi/hello.java
+++ src/main/java/wyc/git_xuexi/hello.java
@@ -4,6 +4,5 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello,world!");
         System.out.println("test");
-        System.out.println("branch dev");
     }
 }
