72c3a28d9d70ce2edfc1fa57f5e1f052af2144cb
wyc <3361135241@qq.com>
2026-05-24 14:57:13 +0800

    分支dev

--- src/main/java/wyc/git_xuexi/hello.java
+++ src/main/java/wyc/git_xuexi/hello.java
@@ -4,5 +4,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello,world!");
         System.out.println("test");
+        System.out.println("branch dev");
     }
 }
